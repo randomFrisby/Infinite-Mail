5055f22542c6d0796170b26787de00ab79c9cfc5
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2022-11-11 00:10:19 +0530

    file Update

---
+++ scripts/SignUp.js
@@ -0,0 +1,14 @@
+function openInput(){
+   
+    document.getElementById("hide-div").style.visibility = "visible";
+
+    document.getElementById("sig-up").style.visibility = "hidden"; 
+
+    document.getElementById("detail").style.visibility = "hidden"; 
+
+    let cont = document.getElementById("contanir2");
+    cont.style.marginTop  = "280px"
+
+   
+    
+}
